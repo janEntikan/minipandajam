41e40eb5743c4cc7049b1296292fcbac75fa7dfd
Amritanshu Shandilya <sandyamritanshu9715@gmail.com>
2023-03-20 23:38:14 +0530

    Created some function definition for bullet physics

--- game/hell.py
+++ game/hell.py
@@ -1,10 +1,17 @@
 from panda3d.core import NodePath, CardMaker
+from panda3d.core import Vec3
+
+# importing player class as we will be using some of its functionalities
+from player import Player
 
 # makes bullets and checks for collisions
 class Hell:
     def __init__(self):
         self.bullet_kinds = {}
         self.bullets_on_screen = []
+        self.velocity = 0 # speed of bullet
+
+        self.acc_factor = 0 # For taking acceleration of bullet
 
         self.bullet = NodePath("bullet")
         bullet_graphic = self.bullet.attach_new_node(CardMaker("bullet").generate())
@@ -27,6 +34,23 @@ class Hell:
         self.bullets_on_screen.append(bullet)
 
     def is_hit(self, who):
+        # Function for checking did bullet hit the target
         return False
+    
+    def bullet_acc(self, acc):
+        # accelerates or decelerates bullet based on the value of acc
+        self.velocity += acc * base.clock.dt
+        return
 
+    def bullet_physics(self):
+        # bullet becomes slower if player slows down
+        
+        """How to detect change in velocity of player?"""
+        old_vel = Player.velocity # Get player's  original velocity
+        Player.update()
+        new_vel = Player.velocity # Get player's velocity after updating
 
+        self.acc_factor = (new_vel - old_vel)/ base.clock.dt
+        self.bullet_acc(acc=self.acc_factor)
+        return
+    
