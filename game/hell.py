0d8894d8ec96ab4934a63227c82c8b6c95058afe
entikan <momojo@rocketship.com>
2023-03-20 12:38:00 +0100

    add some bullets

--- game/hell.py
+++ game/hell.py
@@ -1,6 +1,31 @@
+from panda3d.core import NodePath, CardMaker
+
+# makes bullets and checks for collisions
 class Hell:
-    # makes bullets and checks for collisions
-    pass
+    def __init__(self):
+        self.bullet_kinds = {}
+        self.bullets_on_screen = []
+
+        self.bullet = NodePath("bullet")
+        bullet_graphic = self.bullet.attach_new_node(CardMaker("bullet").generate())
+        bullet_graphic.set_p(-90)
+        bullet_graphic.set_scale((0.1,0.2,0.1))
+        bullet_graphic.set_pos(0.4, 0.5, 0)
+        base.task_mgr.add(self.update)
+
+    def update(self, task):
+        for bullet in self.bullets_on_screen:
+            bullet.set_y(bullet, 20*game.clock.dt)
+            if abs(bullet.get_y()) > 20:
+                self.bullets_on_screen.remove(bullet)
+                bullet.detach_node()
+        return task.cont
+
+    def spawn_bullet(self, pos, kind=None, mask=0):
+        bullet = self.bullet.copy_to(render)
+        bullet.set_pos(pos)
+        self.bullets_on_screen.append(bullet)
+
 
 
 
--- game/player.py
+++ game/player.py
@@ -13,12 +13,21 @@ class Player:
         card.set_transparency(True)
         self.velocity = Vec3()
         base.task_mgr.add(self.update)
+        self.cooldown = 0
+        self.cooling = 0.1
 
     def update(self, task):
         buttons = base.device_listener.read_context("player")
         motion = Vec3(buttons["movement"].x, buttons["movement"].y, 0)
         self.velocity += motion*128*base.clock.dt
-        self.root.set_pos(self.root, self.velocity*base.clock.dt)
-        self.velocity *= base.clock.dt**0.04
-        game.clock.update_slowdown(motion.length())
+        total_vel = self.velocity*base.clock.dt
+        game.clock.update_slowdown(total_vel.length()*3)
+        self.root.set_pos(self.root, total_vel)
+        self.velocity *= base.clock.dt**0.03
+
+        self.cooldown += game.clock.dt
+        if self.cooldown >= self.cooling:
+            self.cooldown -= self.cooling
+            game.hell.spawn_bullet(self.root.get_pos())
+
         return task.cont
